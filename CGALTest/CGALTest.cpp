a08b0f9b34aee786f21a7b0627d3e8c8ab28ef4a
YonatanKoral <yonatan.koral83@gmail.com>
2019-01-10 05:19:46 +0200

    Add project files.

---
+++ CGALTest/CGALTest.cpp
@@ -0,0 +1,12 @@
+﻿// CGALTest.cpp : Defines the entry point for the application.
+//
+
+#include "CGALTest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ CGALTest/CGALTest.h
@@ -0,0 +1,8 @@
+﻿// CGALTest.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
